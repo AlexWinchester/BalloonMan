8f494d92b4ff6c0bcb84af8885adfad9dbcbbabe
gudcandy <63989202+gudcandy@users.noreply.github.com>
2020-05-05 02:02:26 -0500

    enemy melee/animation/sprites
    i had to copy folder to main folder, could not figure out how to merge to overwrite old files. balloonman is very large but when you hit play hes regular size. need to figure out how to resize him properly.

---
+++ balloon man/Assets/Script/PlayerAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+    public float attackRange = 0.5f;
+    public int attackDamage = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+
+        // Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
